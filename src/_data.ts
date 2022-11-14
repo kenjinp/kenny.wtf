96d136d9d28aba3ce77e3de53b5a14aaafa2a5f4
Kenneth Pirman <github@kenny.wtf>
2022-11-15 00:38:37 +0100

    fix stuff

--- src/_data.ts
+++ src/_data.ts
@@ -1,11 +1,3 @@
-
-async function getGitHash () {
-  const cmd = ["git", "rev-parse", "HEAD"];
-  const p = Deno.run({ cmd });
-  return await p.status().toString().trim()
-}
-
 export const title = "Kenny.wtf"
 export const layout = "layout.njk"
 export const deploymentDate = new Date();
-export const deploymentGitHash = getGitHash()
