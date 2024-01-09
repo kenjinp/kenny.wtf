b4fec920ab8eecfcf09be8b1cef72c0841f1e55f
Kenneth Pirman <github@kenny.wtf>
2024-01-09 17:58:54 -0500

    feat: add plausible and table of contents

--- apps/astro/tailwind.config.cjs
+++ apps/astro/tailwind.config.cjs
@@ -13,9 +13,9 @@ module.exports = {
   theme: {
     // Remove the following screen breakpoint or add other breakpoints
     // if one breakpoint is not enough for you
-    screens: {
-      sm: "640px",
-    },
+    // screens: {
+    //   sm: "640px",
+    // },
 
     // Uncomment the following extend
     // if existing Tailwind color palette will be used
