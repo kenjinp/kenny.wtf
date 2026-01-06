ed3258eb91340396b59b7da271ea0e6382351ec5
Kenny Pirman <kenny.pirman@character.ai>
2026-01-06 12:41:25 +0100

    chore: use oxlint and oxfmt

--- apps/astro/src/content/config.ts
+++ apps/astro/src/content/config.ts
@@ -13,7 +13,7 @@ const blog = defineCollection({
       draft: z.boolean().optional(),
       tags: z.array(z.string()).default(["others"]),
       ogImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
+        .refine((img) => img.width >= 1200 && img.height >= 630, {
           message: "OpenGraph image must be at least 1200 X 630 pixels!",
         })
         .or(z.string())
@@ -37,13 +37,13 @@ const projects = defineCollection({
       tags: z.array(z.string()).default(["others"]),
       stars: z.number().optional(),
       ogImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
+        .refine((img) => img.width >= 1200 && img.height >= 630, {
           message: "OpenGraph image must be at least 1200 X 630 pixels!",
         })
         .or(z.string())
         .optional(),
       heroImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
+        .refine((img) => img.width >= 1200 && img.height >= 630, {
           message: "OpenGraph image must be at least 1200 X 630 pixels!",
         })
         .or(z.string())
--- apps/astro/src/utils/getUniqueTags.ts
+++ apps/astro/src/utils/getUniqueTags.ts
@@ -4,11 +4,11 @@ import type { CollectionEntry } from "astro:content";
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
   const filteredPosts = posts.filter(({ data }) => !data.draft);
   const tags: string[] = filteredPosts
-    .flatMap(post => post.data.tags)
-    .map(tag => slugifyStr(tag))
+    .flatMap((post) => post.data.tags)
+    .map((tag) => slugifyStr(tag))
     .filter(
       (value: string, index: number, self: string[]) =>
-        self.indexOf(value) === index
+        self.indexOf(value) === index,
     )
     .sort((tagA: string, tagB: string) => tagA.localeCompare(tagB));
   return tags;
