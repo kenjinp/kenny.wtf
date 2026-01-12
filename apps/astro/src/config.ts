2c1ed661e044b28151f87b0f9c28f4bde0c65863
Kenny Pirman <kenny.pirman@character.ai>
2026-01-12 17:00:37 +0100

    🐛 Fix project list page

--- apps/astro/src/config.ts
+++ apps/astro/src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "Kenneth Pirman",
   ogImage: "/hello-worlds.png",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 10,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
